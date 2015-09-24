69be2aed414f9a90491b0adefd9af9f018e61002
Fredrik Wikstrom <fredrik@a500.org>
2015-09-24 19:45:21 +0300

    Replaced strlcpy/strlcat code.

--- amigaos_support/strlcpy.c
+++ amigaos_support/strlcpy.c
@@ -1,57 +1,81 @@
-/**
- * Copyright (c) 2015 Fredrik Wikstrom <fredrik@a500.org>
+/*
+ * Copyright (c) 1998 Todd C. Miller <Todd.Miller@courtesan.com>
+ * All rights reserved.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <string.h>
 
 #ifndef __AROS__
-size_t strlcpy(char *dst, const char *src, size_t dst_size) {
-	char *dst_start = dst;
-	char *dst_end = dst_start + dst_size;
+size_t strlcpy(char *dst, const char *src, size_t siz) {
+	register char *d = dst;
+	register const char *s = src;
+	register size_t n = siz;
 
-	if (dst_end > dst) {
-		while ((*dst = *src) != '\0') {
-			if (++dst == dst_end) {
-				*--dst = '\0';
+	/* Copy as many bytes as will fit */
+	if (n != 0 && --n != 0) {
+		do {
+			if ((*d++ = *s++) == 0)
 				break;
-			}
-			src++;
-		}
+		} while (--n != 0);
 	}
-	dst += strlen(src);
-	return dst - dst_start;
+
+	/* Not enough room in dst, add NUL and traverse rest of src */
+	if (n == 0) {
+		if (siz != 0)
+			*d = '\0'; /* NUL-terminate dst */
+		while (*s++)
+			;
+	}
+
+	return s - src - 1; /* count does not include NUL */
 }
 
-size_t strlcat(char *dst, const char *src, size_t dst_size) {
-	char *dst_start = dst;
-	char *dst_end = dst_start + dst_size;
+size_t strlcat(char *dst, const char *src, size_t siz) {
+	register char *d = dst;
+	register const char *s = src;
+	register size_t n = siz;
+	size_t dlen;
 
-	dst += strlen(dst);
-	if (dst_end > dst) {
-		while ((*dst = *src) != '\0') {
-			if (++dst == dst_end) {
-				*--dst = '\0';
-				break;
-			}
-			src++;
+	/* Find the end of dst and adjust bytes left but don't go past end */
+	while (n-- != 0 && *d != '\0')
+		d++;
+	dlen = d - dst;
+	n = siz - dlen;
+
+	if (n == 0)
+		return dlen + strlen(s);
+	while (*s != '\0') {
+		if (n != 1) {
+			*d++ = *s;
+			n--;
 		}
+		s++;
 	}
-	dst += strlen(src);
-	return dst - dst_start;
+	*d = '\0';
+
+	return dlen + (s - src); /* count does not include NUL */
 }
 #endif
 
