09af17cfba1068830e25843c9c2b7342b937ee7e
Fredrik Wikstrom <fredrik@a500.org>
2015-12-25 13:32:28 +0200

    ntfs3g-handler 53.3 (25.12.2015)
    - Replaced kprintf() with KPutStr() in vdebugf().
    
    - Replaced sscanf() with a strtol() wrapper.

--- NTFileSystem3G_rev.h
+++ NTFileSystem3G_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		53
-#define REVISION	2
-#define DATE		"3.8.2015"
-#define VERS		"NTFileSystem3G 53.2"
-#define VSTRING		"NTFileSystem3G 53.2 (3.8.2015)\r\n"
-#define VERSTAG		"\0$VER: NTFileSystem3G 53.2 (3.8.2015)"
+#define REVISION	3
+#define DATE		"25.12.2015"
+#define VERS		"NTFileSystem3G 53.3"
+#define VSTRING		"NTFileSystem3G 53.3 (25.12.2015)\r\n"
+#define VERSTAG		"\0$VER: NTFileSystem3G 53.3 (25.12.2015)"
--- ntfs3g-handler_rev.h
+++ ntfs3g-handler_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		53
-#define REVISION	2
-#define DATE		"3.8.2015"
-#define VERS		"ntfs3g-handler 53.2"
-#define VSTRING		"ntfs3g-handler 53.2 (3.8.2015)\r\n"
-#define VERSTAG		"\0$VER: ntfs3g-handler 53.2 (3.8.2015)"
+#define REVISION	3
+#define DATE		"25.12.2015"
+#define VERS		"ntfs3g-handler 53.3"
+#define VSTRING		"ntfs3g-handler 53.3 (25.12.2015)\r\n"
+#define VERSTAG		"\0$VER: ntfs3g-handler 53.3 (25.12.2015)"
