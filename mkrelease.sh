9891ed21947f394055a775f0af6cd13d2915c6bc
Fredrik Wikstrom <fredrik@a500.org>
2015-12-27 10:54:40 +0200

    Added filesysbox.library requirement in readme.

--- mkrelease.sh
+++ mkrelease.sh
@@ -21,8 +21,9 @@ cp -p COPYING ${DESTDIR}/ntfs3g-${NUMVERS}
 cp -p releasenotes ${DESTDIR}/ntfs3g-${NUMVERS}
 cp -p ntfs3g-handler ${DESTDIR}/ntfs3g-${NUMVERS}/L
 
-sed -i "s/^Version:      xx.xx/Version:      ${NUMVERS}/" ${DESTDIR}/ntfs3g-${NUMVERS}/ntfs3g.readme
-sed -i "s/^Architecture: xxx/Architecture: ${HOST}/" ${DESTDIR}/ntfs3g-${NUMVERS}/ntfs3g.readme
+sed -i "s_^Version:      xx.xx_Version:      ${NUMVERS}_" ${DESTDIR}/ntfs3g-${NUMVERS}/ntfs3g.readme
+sed -i "s_^Requires:     xxx_Requires:     util/libs/filesysbox.${HOST}.lha_" ${DESTDIR}/ntfs3g-${NUMVERS}/ntfs3g.readme
+sed -i "s_^Architecture: xxx_Architecture: ${HOST}_" ${DESTDIR}/ntfs3g-${NUMVERS}/ntfs3g.readme
 
 rm -f ntfs3g.${HOST}.7z
 7za u ntfs3g.${HOST}.7z ./${DESTDIR}/ntfs3g-${NUMVERS}
