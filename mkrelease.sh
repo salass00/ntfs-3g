b42e65dbf0ed32b4657f6dfd321229988e89ade5
Fredrik Wikstrom <fredrik@a500.org>
2016-10-17 15:07:57 +0300

    The Aminet readme header is now added on by the mkrelease.sh script.

--- mkrelease.sh
+++ mkrelease.sh
@@ -16,14 +16,19 @@ NUMVERS=`echo "${FULLVERS}" | cut -d' ' -f2`
 rm -rf ${DESTDIR}
 mkdir -p ${DESTDIR}/ntfs3g-${NUMVERS}/L
 
-cp -p ntfs3g.readme ${DESTDIR}/ntfs3g-${NUMVERS}
 cp -p COPYING ${DESTDIR}/ntfs3g-${NUMVERS}
 cp -p releasenotes ${DESTDIR}/ntfs3g-${NUMVERS}
 cp -p ntfs3g-handler ${DESTDIR}/ntfs3g-${NUMVERS}/L
 
-sed -i "s_^Version:      xx.xx_Version:      ${NUMVERS}_" ${DESTDIR}/ntfs3g-${NUMVERS}/ntfs3g.readme
-sed -i "s_^Requires:     xxx_Requires:     util/libs/filesysbox.${HOST}.lha_" ${DESTDIR}/ntfs3g-${NUMVERS}/ntfs3g.readme
-sed -i "s_^Architecture: xxx_Architecture: ${HOST}_" ${DESTDIR}/ntfs3g-${NUMVERS}/ntfs3g.readme
+echo "Short:        A port of Tuxera's NTFS-3G file system" > ${DESTDIR}/ntfs3g-${NUMVERS}/ntfs3g.readme
+echo "Author:       Fredrik Wikstrom" >> ${DESTDIR}/ntfs3g-${NUMVERS}/ntfs3g.readme
+echo "Uploader:     Fredrik Wikstrom <fredrik@a500.org>" >> ${DESTDIR}/ntfs3g-${NUMVERS}/ntfs3g.readme
+echo "Type:         disk/misc" >> ${DESTDIR}/ntfs3g-${NUMVERS}/ntfs3g.readme
+echo "Version:      ${NUMVERS}" >> ${DESTDIR}/ntfs3g-${NUMVERS}/ntfs3g.readme
+echo "Requires:     util/libs/filesysbox.${HOST}.lha" >> ${DESTDIR}/ntfs3g-${NUMVERS}/ntfs3g.readme
+echo "Architecture: ${HOST}" >> ${DESTDIR}/ntfs3g-${NUMVERS}/ntfs3g.readme
+echo "" >> ${DESTDIR}/ntfs3g-${NUMVERS}/ntfs3g.readme
+cat README >> ${DESTDIR}/ntfs3g-${NUMVERS}/ntfs3g.readme
 
 rm -f ntfs3g.${HOST}.7z
 7za u ntfs3g.${HOST}.7z ./${DESTDIR}/ntfs3g-${NUMVERS}
