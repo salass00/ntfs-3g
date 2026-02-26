7acab85192bbd6af67af3343ccfad32489debdf9
Fredrik Wikstrom <fredrik@a500.org>
2026-02-26 17:04:16 +0200

    Rewrote startup code for newer AROS SDK.

--- src/ntfs3g-startup_amigaos.c
+++ src/ntfs3g-startup_amigaos.c
@@ -17,25 +17,17 @@
 
 #include <dos/dosextens.h>
 #include <proto/exec.h>
+#include <proto/dos.h>
 #include <proto/filesysbox.h>
 #include <SDI/SDI_compiler.h>
 #include "ntfs3g-handler_rev.h"
 
-static const char USED verstag[] = VERSTAG;
-
-#define MIN_OS_VERSION    39
-#define MIN_AROSC_VERSION 41
-#define MIN_FBX_VERSION   53
-
 struct ExecBase *SysBase;
-struct DOSBase *DOSBase;
+struct DosLibrary *DOSBase;
 struct UtilityBase *UtilityBase;
-#ifndef __AROS__
-struct DOSBase *__DOSBase;
-struct UtilityBase *__UtilityBase;
-#endif
 #ifdef __AROS__
-struct Library *aroscbase;
+struct Library *StdlibBase;
+struct Library *CrtBase;
 #endif
 struct Library *FileSysBoxBase;
 
@@ -45,10 +37,10 @@ struct Library *FileSysBoxBase;
 
 //#define DEBUG
 #ifdef DEBUG
-	#include <debugf.h>
-	#define DEBUGF(str,args...) debugf(str, ## args)
+# include <debugf.h>
+# define DEBUGF(str,args...) debugf(str, ## args)
 #else
-	#define DEBUGF(str,args...)
+# define DEBUGF(str,args...)
 #endif
 
 extern int setup_malloc(void);
@@ -56,14 +48,32 @@ extern int cleanup_malloc(void);
 
 extern int ntfs3g_main(struct Message *msg);
 
+static const TEXT vstring[];
+static const TEXT dosName[];
+static const TEXT utilityName[];
 #ifdef __AROS__
-__startup static AROS_PROCH(startup, argstr, argsize, sysbase) {
-	AROS_PROCFUNC_INIT
-#else
-int startup(void) {
+static const TEXT stdlibName[];
+static const TEXT crtName[];
 #endif
-	int rc = RETURN_FAIL, error = 0;
-	struct Process *thisproc;
+static const TEXT filesysboxName[];
+
+#ifdef __AROS__
+__startup AROS_UFH3(int, startup,
+	AROS_UFHA(STRPTR, argstr, A0),
+	AROS_UFHA(ULONG, arglen, D0),
+	AROS_UFHA(struct ExecBase *, sysbase, A6)
+)
+{
+	AROS_USERFUNC_INIT
+#else
+int startup(void)
+{
+#endif
+	struct Process   *me;
+	struct Message   *msg;
+	struct DosPacket *pkt = NULL;
+	int               rc = RETURN_ERROR;
+	struct MsgPort   *port = NULL;
 
 #ifdef __AROS__
 	SysBase = sysbase;
@@ -71,89 +81,128 @@ int startup(void) {
 	SysBase = *(struct ExecBase **)4;
 #endif
 
-	DEBUGF("ntfs3g_startup: got execbase %#p\n", SysBase);
-
-	thisproc = (struct Process *)FindTask(NULL);
-
-	if (thisproc->pr_CLI != ZERO) {
-		DEBUGF("ntfs3g_startup: CLI startup not supported\n");
-		return rc;
+	if (!setup_malloc())
+	{
+		goto cleanup;
 	}
 
-	struct MsgPort *port = &thisproc->pr_MsgPort;
+	DOSBase = (struct DosLibrary *)OpenLibrary(dosName, 39);
+	if (DOSBase == NULL)
+	{
+		goto cleanup;
+	}
 
-	DEBUGF("ntfs3g_startup: waiting at port %#p\n", port);
+	UtilityBase = (struct UtilityBase *)OpenLibrary(utilityName, 39);
+	if (UtilityBase == NULL)
+	{
+		goto cleanup;
+	}
 
+#ifdef __AROS__
+	StdlibBase = OpenLibrary(stdlibName, 1);
+	if (StdlibBase == NULL)
+	{
+		goto cleanup;
+	}
+	CrtBase = OpenLibrary(crtName, 2);
+	if (CrtBase == NULL)
+	{
+		goto cleanup;
+	}
+#endif
+
+	me = (struct Process *)FindTask(NULL);
+	if (me->pr_CLI != 0)
+	{
+		PutStr(vstring);
+		rc = RETURN_OK;
+		goto cleanup;
+	}
+
+	port = &me->pr_MsgPort;
 	WaitPort(port);
-	struct Message *msg = GetMsg(port);
-	if (msg == NULL) goto end;
+	msg = GetMsg(port);
+	if (msg == NULL) goto cleanup;
 
-	if (msg->mn_Node.ln_Name == NULL) {
-		DEBUGF("ntfs3g_startup: WB startup not supported\n");
+	if (msg->mn_Node.ln_Name == NULL)
+	{
+		rc = RETURN_FAIL;
 		Forbid();
 		ReplyMsg(msg);
-		return rc;
+		goto cleanup;
 	}
 
-	DEBUGF("ntfs3g_startup: got msg %#p\n", msg);
+	pkt = (struct DosPacket *)msg->mn_Node.ln_Name;
 
-	if (!setup_malloc()) goto end;
+	FileSysBoxBase = OpenLibrary(filesysboxName, 54);
+	if (FileSysBoxBase == NULL)
+	{
+		goto cleanup;
+	}
 
-	DOSBase = (struct DOSBase *)OpenLibrary((CONST_STRPTR)"dos.library", MIN_OS_VERSION);
-	if (DOSBase == NULL) goto end;
-#ifndef __AROS__
-	__DOSBase = DOSBase;
-#endif
+	if (ntfs3g_main(msg) == 0)
+	{
+		rc = RETURN_OK;
+	}
 
-	UtilityBase = (struct UtilityBase *)OpenLibrary((CONST_STRPTR)"utility.library", MIN_OS_VERSION);
-	if (UtilityBase == NULL) goto end;
-#ifndef __AROS__
-	__UtilityBase = UtilityBase;
-#endif
+	/* Set to NULL so we don't reply the packet twice */
+	pkt = NULL;
 
-#ifdef __AROS__
-	aroscbase = OpenLibrary((CONST_STRPTR)"arosc.library", MIN_AROSC_VERSION);
-	if (aroscbase == NULL) goto end;
-#endif
+cleanup:
 
-	FileSysBoxBase = OpenLibrary((CONST_STRPTR)"filesysbox.library", MIN_FBX_VERSION);
-	if (FileSysBoxBase == NULL) goto end;
+	if (FileSysBoxBase != NULL)
+	{
+		CloseLibrary(FileSysBoxBase);
+		FileSysBoxBase = NULL;
+	}
 
-	error = ntfs3g_main(msg);
-	msg = NULL;
-	if (!error) rc = RETURN_OK;
-
-end:
-	DEBUGF("ntfs3g_startup: shutting down (err %d, rc %d)\n", error, rc);
-
-	CloseLibrary(FileSysBoxBase);
+	if (pkt != NULL)
+	{
+		ReplyPkt(pkt, DOSFALSE, ERROR_INVALID_RESIDENT_LIBRARY);
+		pkt = NULL;
+	}
 
 #ifdef __AROS__
-	CloseLibrary(aroscbase);
+	if (CrtBase != NULL)
+	{
+		CloseLibrary(CrtBase);
+		CrtBase = NULL;
+	}
+	if (StdlibBase != NULL)
+	{
+		CloseLibrary(StdlibBase);
+		StdlibBase = NULL;
+	}
 #endif
 
-	CloseLibrary((struct Library *)UtilityBase);
+	if (UtilityBase != NULL)
+	{
+		CloseLibrary((struct Library *)UtilityBase);
+		DOSBase = NULL;
+	}
 
-	CloseLibrary((struct Library *)DOSBase);
+	if (DOSBase != NULL)
+	{
+		CloseLibrary((struct Library *)DOSBase);
+		DOSBase = NULL;
+	}
 
 	cleanup_malloc();
-
-	if (msg != NULL) {
-		struct DosPacket *pkt = (struct DosPacket *)msg->mn_Node.ln_Name;
-		struct MsgPort *replyport = pkt->dp_Port;
-
-		pkt->dp_Res1 = DOSFALSE;
-		pkt->dp_Res2 = 0;
-
-		PutMsg(replyport, msg);
-	}
-
-	DEBUGF("ntfs3g_startup: bye.\n");
 
 	return rc;
 
 #ifdef __AROS__
-	AROS_PROCFUNC_EXIT
+	AROS_USERFUNC_EXIT
 #endif
 }
+
+static const TEXT USED verstag[] = VERSTAG;
+static const TEXT vstring[] = VSTRING;
+static const TEXT dosName[] = "dos.library";
+static const TEXT utilityName[] = "utility.library";
+#ifdef __AROS__
+static const TEXT stdlibName[] = "stdlib.library";
+static const TEXT crtName[] = "crt.library";
+#endif
+static const TEXT filesysboxName[] = "filesysbox.library";
 
