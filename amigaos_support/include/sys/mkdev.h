92cdafabeb15059d92cf51403941bb916e5f4d6f
Fredrik Wikstrom <fredrik@a500.org>
2026-02-27 19:52:57 +0200

    Don't use only for AROS.

--- amigaos_support/include/sys/mkdev.h
+++ amigaos_support/include/sys/mkdev.h
@@ -3,7 +3,6 @@
 
 #include <sys/types.h>
 
-#ifdef __AROS__
 dev_t makedev(int maj, int min);
 unsigned int major(dev_t dev);
 unsigned int minor(dev_t dev);
@@ -11,7 +10,6 @@ unsigned int minor(dev_t dev);
 #define makedev(maj,min) (((dev_t)(maj) & 0xffff) << 16)|((dev_t)(min) & 0xffff)
 #define major(dev) (((dev) >> 16) & 0xffff)
 #define minor(dev) ((dev) & 0xffff)
-#endif
 
 #endif
 
