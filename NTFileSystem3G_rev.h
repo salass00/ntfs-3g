f34699b937f112b2115956676efe4dab681ef7b5
Fredrik Wikstrom <fredrik@a500.org>
2026-03-03 13:55:41 +0200

    Bumped version to 53.5.

--- NTFileSystem3G_rev.h
+++ NTFileSystem3G_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		53
-#define REVISION	4
-#define DATE		"3.2.2016"
-#define VERS		"NTFileSystem3G 53.4"
-#define VSTRING		"NTFileSystem3G 53.4 (3.2.2016)\r\n"
-#define VERSTAG		"\0$VER: NTFileSystem3G 53.4 (3.2.2016)"
+#define REVISION	5
+#define DATE		"3.3.2026"
+#define VERS		"NTFileSystem3G 53.5"
+#define VSTRING		"NTFileSystem3G 53.5 (3.3.2026)\r\n"
+#define VERSTAG		"\0$VER: NTFileSystem3G 53.5 (3.3.2026)"
--- ntfs3g-handler_rev.h
+++ ntfs3g-handler_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		53
-#define REVISION	4
-#define DATE		"3.2.2016"
-#define VERS		"ntfs3g-handler 53.4"
-#define VSTRING		"ntfs3g-handler 53.4 (3.2.2016)\r\n"
-#define VERSTAG		"\0$VER: ntfs3g-handler 53.4 (3.2.2016)"
+#define REVISION	5
+#define DATE		"3.3.2026"
+#define VERS		"ntfs3g-handler 53.5"
+#define VSTRING		"ntfs3g-handler 53.5 (3.3.2026)\r\n"
+#define VERSTAG		"\0$VER: ntfs3g-handler 53.5 (3.3.2026)"
